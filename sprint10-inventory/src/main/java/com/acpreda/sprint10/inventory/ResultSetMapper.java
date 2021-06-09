10005652577f2bfcc054d6671754c91a0ca48cec
Arturo Cruz <acpreda@gmail.com>
2021-06-08 23:53:00 -0500

    Refactor, cleaning and sprint10-return added

--- sprint10-inventory/src/main/java/com/acpreda/sprint10/inventory/ResultSetMapper.java
+++
@@ -1,9 +0,0 @@
-package com.acpreda.sprint10.inventory;
-
-import java.sql.ResultSet;
-
-public interface ResultSetMapper<T> {
-
-    T map(ResultSet rs, int index);
-
-}
--- sprint10-vfs/src/main/java/com/acpreda/sprint10/vfs/VFS.java
+++
@@ -1,9 +0,0 @@
-package com.acpreda.sprint10.vfs;
-
-public interface VFS {
-
-    void save(VFSFile file);
-    VFSFile find(String name);
-    void addFileToFolder(String file, String folder);
-
-}
